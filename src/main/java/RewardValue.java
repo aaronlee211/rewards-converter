e1178a6f9f6ba325df6642368d78b26da37a093e
Aaron Lee <aaronlee97667@gmail.com>
2023-06-28 13:01:40 -0500

    Added handling for miles to cash conversion

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,8 +1,7 @@
 public class RewardValue {
-    private Double cash;
+    private double cash;
 
     private static final double conversionRate = 0.0035;
-    private Integer miles;
 
     // The two constructors are initialized with the same name and are subject to method overloading
     // if the passed in argument is a double it'll utilize the first one (cashValue) otherwise if
@@ -13,15 +12,19 @@ public class RewardValue {
 
     // instructed to make a constructor to accept miles, but in the RewardsConverter, there is no
     // line to create a new constructor passing in miles, so it is unused
-    public RewardValue(Integer miles) {
-        this.miles = miles;
+    public RewardValue(int milesValue) {
+        this.cash = convertToMoney(milesValue);
     }
 
     // Grabs the cash value that is assigned to the class RewardValue
     // When initialized with new RewardValue(cashValue)
-    public Double getCashValue() { return cash; }
+    public double getCashValue() { return cash; }
 
     // Applies the conversion rate and returns the appropriate mile rewards
     // based on the amount of cash
-    public Double getMilesValue() { return cash / conversionRate; }
+    public double getMilesValue() { return cash / conversionRate; }
+
+    public int convertToMoney(int milesValue) {
+        return (int) (milesValue * conversionRate);
+    }
 }
